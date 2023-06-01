89162be38fa4a3ea62bece65dee1023e0ef7e6a6
seunghwan <sun1238799@naver.com>
2023-06-02 05:42:40 +0900

    refactor: nav바 수직 중앙정렬

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 
 const Nav = () => {
   return (
-    <div className="flex max-w-5xl justify-between p-3 mx-auto">
+    <div className="flex max-w-5xl items-center justify-between p-3 mx-auto my-0">
       <h1 className="text-lg font-bold">Seunghwan`s Blog</h1>
       <div className="flex gap-4">
         {NavList.map((list) => {
